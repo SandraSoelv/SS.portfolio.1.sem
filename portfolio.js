fe0b9b066b7265fa426d1e9a40a0b5641284e47a
SandraSoelv <31647240+SandraSoelv@users.noreply.github.com>
2018-01-07 20:17:40 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function openTabs(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "flex";
+}
